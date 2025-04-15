089f16b0c2d5345b59ee3154aaae4ed638318b76
杨兴明 <18328321642@163.COM>
2025-04-15 09:41:34 +0800

    Initial commit

---
+++ src/test/java/com/th/iqpcmbiz/IqpcmBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th.iqpcmbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqpcmBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
